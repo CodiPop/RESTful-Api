c03758c0929149aa8ec8566decc13ef689ad6b40
Santiago Guerrero Martinez <47578270+CodiPop@users.noreply.github.com>
2022-12-04 14:22:22 -0500

    Until 22

--- BancoRESTful/Identity/Seeds/DefaultRoles.cs
+++ BancoRESTful/Identity/Seeds/DefaultRoles.cs
@@ -15,7 +15,8 @@ namespace Identity.Seeds
         public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             //Seed Roles
-            await roleManager.CreateAsync(new IdentityRole(Roles))
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Basic.ToString()));
         }
     }
 }
